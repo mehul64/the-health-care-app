29b6c1aee9ca5658adecdad09108eedd206cdad6
mehul64 <30364384+mehul64@users.noreply.github.com>
2019-03-05 23:14:48 +0530

    Initial commit

---
+++ app/src/main/java/com/thc/app/Interfaces/MyDataPoint.java
@@ -0,0 +1,36 @@
+package com.thc.app.Interfaces;
+
+import com.jjoe64.graphview.series.DataPointInterface;
+
+import java.util.ArrayList;
+
+/**
+ * Created by HP on 17-06-2017.
+ */
+
+public class MyDataPoint implements DataPointInterface {
+
+    ArrayList<String> statusList;
+    int x;
+    String y;
+
+    public MyDataPoint(int x, String y) {
+        this.x = x;
+        this.y = y;
+
+        statusList=new ArrayList<>();
+        statusList.add("bad");
+        statusList.add("average");
+        statusList.add("good");
+    }
+
+    @Override
+    public double getX() {
+        return x;
+    }
+
+    @Override
+    public double getY() {
+        return statusList.indexOf(y);
+    }
+}
